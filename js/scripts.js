27377ca89bd10290a87ac2ae1e9762cfd459fea4
Andoni Barba <andoni1barba@gmail.com>
2023-08-04 16:04:09 -0400

    Mobile Menu ready

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,19 @@
 const navbarEmail = document.querySelector(".navbar-email");
 const desktopMenu = document.querySelector(".desktop-menu");
+const burgerButton = document.querySelector(".menu");
+const mobileMenu = document.querySelector(".mobile-menu");
 
 
 /*Events*/
 navbarEmail.addEventListener("click",toggleDesktopMenu);
+burgerButton.addEventListener("click",toggleMobileMenu);
 
 /*Funtions */
 function toggleDesktopMenu(event) {
     desktopMenu.classList.toggle("inactive");
+}
+function toggleMobileMenu(event){
+    mobileMenu.classList.toggle("inactive");
 }
 
 console.log("puto")
